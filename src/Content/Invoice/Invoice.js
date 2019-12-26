8bb5eb8648e46f92b3bb768adbbd9f69cbbf5e60
akhilgangula <akhilreddy.gangula96@gmail.com>
2019-12-26 22:40:17 +0530

    print

--- src/Content/Invoice/Invoice.js
+++ src/Content/Invoice/Invoice.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Row, Card, Col, Container } from 'react-bootstrap'
+import { Row, Card, Col, Container, Table } from 'react-bootstrap'
 import './Invoice.css'
 class Invoice extends React.Component {
 
@@ -17,86 +17,97 @@ class Invoice extends React.Component {
                                     <h2>RA Engineers</h2>
                                 </Col>
                             </Row>
-                            <Row className='invoice-header'>
-                                <Col sm={5}>
-                                    <h1>INVOICE</h1>
-                                </Col>
-                                <Col sm={3} style={{ textAlign: 'right' }}>
-                                    +091-9502738995<br />
+                            <Table >
+                                <tbody >
+                                    <tr className='invoice-header'>
+                                        <td><h1>INVOICE</h1></td>
+                                        <td style={{ textAlign: 'right' }}>+091-9502738995<br />
                                     akhilreddy@gangula.com<br />
-                                    mywebsite.com<br />
-                                </Col>
-                                <Col sm={4} style={{ textAlign: 'right' }}>
-                                    5-9-16, 3A, Prashanti Nagar,<br />
+                                    mywebsite.com<br /></td>
+                                        <td style={{ textAlign: 'right' }}>5-9-16, 3A, Prashanti Nagar,<br />
                                     IDA Kukatpally, Kukatpally,<br />
-                                    Hyderabad, Telangana 500072<br />
-                                </Col>
-                            </Row>
+                                    Hyderabad, Telangana 500072<br /></td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                            
                             <br />
-                            <Row>
-                                <Col sm={6}>Date: {new Date().getDate() + '/' + new Date().getMonth() + "/" + new Date().getFullYear()}</Col>
-                                <Col md={{ span: 2, offset: 4 }}>Place: Hyderabad</Col>
-                            </Row>
-                            <Row>
-                                <Col>Time: {new Date().getHours() + ':' + new Date().getMinutes() + ":" + new Date().getSeconds()}</Col>
-
-                            </Row>
+                            <Table>
+                                <tbody>
+                                    <tr>
+                                        <td>Date: {new Date().getDate() + '/' + new Date().getMonth() + "/" + new Date().getFullYear()}</td>
+                                        <td style={{ textAlign: 'right' }}>Place: Hyderabad</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                            
                             <br />
                             <Row>
                                 <Col style={{ textAlign: 'justify' }}>
                                     We are pleased to know that you (<b>{this.props.data.firm}</b>) have placed an order for <b>{this.props.data.product}</b> of
                                     type <b>{this.props.data.type}</b> on <b>{this.props.data.createdOn}</b>. We are happy to say that your order is placed
-        successfully with us. As agreed your shippment will reach {this.props.data.port} on {this.props.data.date}
+        successfully with us. As agreed your shippment will reach <b>{this.props.data.port}</b> on <b>{this.props.data.date}</b>
                                 </Col>
                             </Row>
                             <br />
-                            <Row>
-                                <Col sm={3}>
-                                    <Card.Subtitle className="mb-2 text-muted invoice-subtitles">Billed To</Card.Subtitle>
+                            <Table>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                        <Card.Subtitle className="mb-2 text-muted invoice-subtitles">Billed To</Card.Subtitle>
                                     <div>{this.props.data.firm}</div>
                                     <div>{this.props.data.phone}</div>
                                     <div>{this.props.data.email}</div>
                                     <div>{this.props.data.address}</div>
-                                </Col>
-                                <Col sm={4}>
-                                    <Card.Subtitle className="mb-2 text-muted invoice-subtitles">Invoice #: </Card.Subtitle>
+                                        </td>
+                                        <td>
+                                        <Card.Subtitle className="mb-2 text-muted invoice-subtitles">Invoice #: </Card.Subtitle>
                                     <div>{this.props.invoiceId}</div>
-                                </Col>
-                                <Col md={{ span: 4, offset: 1 }} style={{ textAlign: 'right' }}>
-                                    <Card.Subtitle className="mb-2 text-muted">Invoice Total: </Card.Subtitle>
+                                        </td>
+                                        <td>
+                                        <Card.Subtitle className="mb-2 text-muted">Invoice Total: </Card.Subtitle>
                                     <h2 className="invoice-color">₹{this.props.data.total}</h2>
-                                </Col>
-                            </Row>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                         
                             <br />
                             <hr className='divider' />
                             <br />
                             <Container>
-                                <Row className='invoice-color'>
-                                    <Col sm={4}>Description</Col>
-                                    <Col sm={3} className='invoice-right'>Unit Cost</Col>
-                                    <Col sm={3} className='invoice-right'>Quantity(in tons)</Col>
-                                    <Col sm={2} className='invoice-right'>Amount</Col>
-                                </Row>
-                                <Row>
-                                    <Col sm={4}>
-                                        {this.props.data.product} - {this.props.data.type}
+                                <Table borderless>
+                                    <tbody>
+                                        <tr>
+                                            <td>Description</td>
+                                            <td className='invoice-right'>Unit Cost</td>
+                                            <td className='invoice-right'>Quantity(in tons)</td>
+                                            <td className='invoice-right'>Quantity(in tons)</td>
+                                        </tr>
+                                    
+                                        <tr>
+                                            <td>{this.props.data.product} - {this.props.data.type}
                                         <Card.Subtitle className="text-muted invoice-subtitles" style={{ paddingTop: '5px' }}>
                                             {this.props.data.size}
-                                        </Card.Subtitle>
-                                    </Col>
-                                    <Col sm={3} className='invoice-right'>{this.props.data.price}</Col>
-                                    <Col sm={3} className='invoice-right'>{this.props.data.qty}</Col>
-                                    <Col sm={2} className='invoice-right'>{this.props.data.total}</Col>
-                                </Row>
+                                        </Card.Subtitle></td>
+                                        <td className='invoice-right'>{this.props.data.price}</td>
+                                        <td className='invoice-right'>{this.props.data.qty}</td>
+                                        <td className='invoice-right'>{this.props.data.total}</td>
+                                        </tr>
+                                        <tr>
+                                        <td></td>
+                                        <td></td>
+                                            <td className='invoice-right'>Total</td>
+                                            <td className='invoice-right'>₹{this.props.data.total}</td>
+                                        </tr>
+                                    </tbody>
+
+                                </Table>                                
+                                
                                 <hr />
-                                <Row>
-                                    <Col md={{ span: 2, offset: 8 }} className='invoice-color invoice-right'>
-                                        Total
-                                    </Col>
-                                    <Col md={{ span: 2 }} className='invoice-color invoice-right'>
-                                        ₹{this.props.data.total}
-                                    </Col>
-                                </Row>
+
+                               
+                                
                                 <br/>
                                 <Row className='invoice-header invoice-right'>
                                     <Col>
--- src/Content/Invoice/create.js
+++ src/Content/Invoice/create.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Container, Button, Form, Col, Row, InputGroup, Card, ListGroup } from 'react-bootstrap'
 import { GenerateID, NumberConvertor } from './../../lib/Utils'
 import Invoice from "./Invoice";
+import ReactToPrint from 'react-to-print';
+
+
 class Create extends React.Component {
 
     state = {
@@ -9,7 +12,7 @@ class Create extends React.Component {
         clientId: "CUST-3563456",
         getNow: false, //turn this off
         generatePage: false,
-        invoiceId : "null",
+        invoiceId: "INV-" + GenerateID(),
         data: {
             Name: "Akhil",
             email: "akhilreddy@gmail.com",
@@ -21,95 +24,105 @@ class Create extends React.Component {
             size: "small",
             port: "Mumbai",
             date: 'Feb-Mar 2020',
-            createdOn : '26/12/2019',
+            createdOn: '26/12/2019',
             qty: 100,
             price: 2.73,
             total: NumberConvertor(273000), //qty*price*1000,
+
+        },
+    }
+
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Form.Label column sm="2">
+                        INVOICE ID:</Form.Label>
+                    <Col sm="4">
+                        <Form.Control plaintext readOnly defaultValue={this.state.invoiceId} />
+                    </Col>
+                    <Form.Label column sm="2">
+                        Request ID:</Form.Label>
+                    <Col sm="4">
+                        <InputGroup className="mb-3">
+                            <InputGroup.Prepend>
+                                <InputGroup.Text id="basic-addon1">REQUEST-</InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <Form.Control type="text" placeholder="Start typing request ID" onChange={this.validateReqId} />
+                        </InputGroup>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={4}>
+                        <PopulateData description={this.state.clientId} header="Client Card" data={this.compileData(this.state.getNow, this.state.requestId, "Client")} />
+                    </Col>
+                    <Col sm={4}>
+                        <PopulateData description={this.state.requestId} header="Request Card" data={this.compileData(this.state.getNow, this.state.requestId, "Request")} />
+                    </Col>
+                    <Col sm={4}>
+                        <PopulateData description={this.state.requestId} header="Price Card" data={this.compileData(this.state.getNow, this.state.requestId, "Price")} />
+                        <Button variant="outline-success" onClick={() => this.setState({ generatePage: true })}>Generate</Button>
+                    </Col>
+                </Row>
+                <div>
+                    <ReactToPrint
+                        trigger={() => <a href="#">Print this out!</a>}
+                        content={() => this.componentRef}
+                    />
+                    
+                    <Invoice ref={el => (this.componentRef = el)} show={this.state.generatePage} data={this.state.data} invoiceId={this.state.invoiceId} />
+                    
+                </div>
+            </Container>
+        )
+    }
+    
+    compileData = (compute, requestId, collection) => {
+        if (!compute) {
+            return ""
+        }
+        switch (collection) {
+            case "Client":
+
+                return {
+                    Name: "Akhil",
+                    email: "akhilreddy@gmail.com",
+                    phone: 9502738995,
+                    Firm: "RA Engineers",
+                    address: "My address",
+                }
+            case "Request":
+                return {
+                    product: "GYPSUM",
+                    type: "LUMPS",
+                    size: "small",
+                    port: "Mumbai",
+                    date: 'Feb-Mar 2020'
+                }
+            case "Price":
+                return {
+                    qty: 100,
+                    price: 2.73,
+                    total: NumberConvertor(273000), //qty*price*1000,
+                }
+            default:
+                break;
+        }
+    }
+
+
+    validateReqId = (event) => {
+        if (event.target.value != "") {
+            //search db with startswith
+
+
+            //don't generate if entry already present
+            //assuming match is found
+            this.setState({ getNow: true })
+        }
     }
 }
 
-render() {
-    return (
-        <Container>
-            <Row>
-                <Form.Label column sm="2">
-                    INVOICE ID:</Form.Label>
-                <Col sm="4">
-                    <Form.Control plaintext readOnly defaultValue={"INV-" + GenerateID()} />
-                </Col>
-                <Form.Label column sm="2">
-                    Request ID:</Form.Label>
-                <Col sm="4">
-                    <InputGroup className="mb-3">
-                        <InputGroup.Prepend>
-                            <InputGroup.Text id="basic-addon1">REQUEST-</InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <Form.Control type="text" placeholder="Start typing request ID" onChange={this.validateReqId} />
-                    </InputGroup>
-                </Col>
-            </Row>
-            <Row>
-                <Col sm={4}>
-                    <PopulateData description={this.state.clientId} header="Client Card" data={this.compileData(this.state.getNow, this.state.requestId, "Client")} />
-                </Col>
-                <Col sm={4}>
-                    <PopulateData description={this.state.requestId} header="Request Card" data={this.compileData(this.state.getNow, this.state.requestId, "Request")} />
-                </Col>
-                <Col sm={4}>
-                    <PopulateData description={this.state.requestId} header="Price Card" data={this.compileData(this.state.getNow, this.state.requestId, "Price")} />
-                    <Button variant="outline-success" onClick={()=>this.setState({generatePage:true})}>Generate</Button>
-                </Col>
-            </Row>
-            <Invoice show={this.state.generatePage} data={this.state.data} invoiceId={this.state.invoiceId}/>
-        </Container>
-    )
-}
-
-compileData = (compute, requestId, collection) => {
-    if (!compute) {
-        return ""
-    }
-    switch (collection) {
-        case "Client":
-
-            return {
-                Name: "Akhil",
-                email: "akhilreddy@gmail.com",
-                phone: 9502738995,
-                Firm: "RA Engineers",
-                address: "My address",
-            }
-        case "Request":
-            return {
-                product: "GYPSUM",
-                type: "LUMPS",
-                size: "small",
-                port: "Mumbai",
-                date: 'Feb-Mar 2020'
-            }
-        case "Price":
-            return {
-                qty: 100,
-                price: 2.73,
-                total: NumberConvertor(273000), //qty*price*1000,
-            }
-        default:
-            break;
-    }
-}
-
-
-validateReqId = (event) => {
-    if (event.target.value != "") {
-        //search db with startswith
-
-
-        //don't generate if entry already present
-        //assuming match is found
-        this.setState({ getNow: true })
-    }
-}
-}
 
 class PopulateData extends React.Component {
 
